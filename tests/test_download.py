68a91e44ef9bcc52c476f261f0a84ad979ffacf3
Kongtap Arunlakvilart <kongtap@fintech.co.th>
2024-09-05 00:13:31 +0700

    test: download

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,10 +1,19 @@
 import pandas as pd
+import pytest
 
 from binance_data_collector.download import download_klines
 
 
-def test_download_klines():
-    csv_file = download_klines(symbol="BTCUSDT", interval="1h")
+@pytest.mark.parametrize("symbol", ["BTCUSDT", "ETHUSDT"])
+@pytest.mark.parametrize("interval", ["15m", "1h", "1d"])
+@pytest.mark.parametrize("trading_type", ["spot", "um", "cm"])
+# @pytest.mark.parametrize(
+#     "market_data_type", ["klines", "indexPriceKlines", "markPriceKlines", "premiumIndexKlines", "metrics"]
+# )
+def test_download_klines(symbol: str, interval: str, trading_type: str, market_data_type: str = "klines"):
+    csv_file = download_klines(
+        symbol=symbol, interval=interval, trading_type=trading_type, market_data_type=market_data_type
+    )
 
     df = pd.read_csv(csv_file, index_col=0)
 
